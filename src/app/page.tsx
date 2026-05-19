fa2635b955420a5eaf40cb51f9a36183adce4471
jsprogram2014 <jsprogram2014@gmail.com>
2026-05-19 10:48:22 -0400

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import { Input } from "@/components/ui/input";
+
+
 export default function Home() {
+
+  const [email,setEmail]=useState("");
+  const [name,setName]=useState("");
+  const [password,setPassword]=useState("");
+
   return (
-    <Button variant="custom">
-      Click Me
-    </Button>
+    <div>
+      <Input placeholder="name" value={name} onChange={(e)=>e.target.value}/>
+    </div>
   );
 }
